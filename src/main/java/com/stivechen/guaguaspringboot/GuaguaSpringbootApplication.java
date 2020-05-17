27c021acf24a5f71927fd483de2d7b268d9b3836
stivechen <stivechen1989@gmail.com>
2020-05-17 22:15:08 +0800

    init project

---
+++ src/main/java/com/stivechen/guaguaspringboot/GuaguaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.stivechen.guaguaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuaguaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuaguaSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stivechen/guaguaspringboot/GuaguaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stivechen.guaguaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuaguaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
